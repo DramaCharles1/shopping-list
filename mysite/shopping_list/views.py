a8a35db870a8985fa5166ddb9e0b8feaf1bc0ccc
Richard Stuart <richard.niklas.stuart@gmail.com>
2024-01-09 22:38:23 +0100

    Call index.html from IndexView in views.py
    indexView handles queries to the db and is called via urls.py from index.html

--- mysite/shopping_list/views.py
+++ mysite/shopping_list/views.py
@@ -1,5 +1,15 @@
+from django.views import generic
 from django.http import HttpResponse
+from django.utils import timezone
 
+from .models import Item
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the shopping list index.")
+class IndexView(generic.ListView):
+    template_name = "shopping_list/index.html"
+    context_object_name = "latest_shopping_list"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Item.objects.filter(date_added__lte=timezone.now())
