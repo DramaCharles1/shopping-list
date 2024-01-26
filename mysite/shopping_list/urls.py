e55331a66faec1cc6a93cde7ef429c3ca46280f8
Richard Stuart <richard.niklas.stuart@gmail.com>
2024-01-26 23:01:15 +0100

    Add ItemListCreateView to shopping_list urls
    The API endpoint for adding an item to the shopping list. Accessed from
    both shopping_list/Item and api/Item. Should only be form api/Item?

--- mysite/shopping_list/urls.py
+++ mysite/shopping_list/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
 from . import views
+from .views import ItemlListCreateView
+
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path('Item/', ItemlListCreateView.as_view(), name='item-list-create'),
 ]
