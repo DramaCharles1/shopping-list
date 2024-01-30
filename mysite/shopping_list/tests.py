3c9df7e23398e39afd015e28af6fb9a813229106
Richard Stuart <richard.niklas.stuart@gmail.com>
2024-01-30 23:28:31 +0100

    Test: no items in shopping list
    If the shopping list is empty a "No items are available." message
    should be displayed in the index view.

--- mysite/shopping_list/tests.py
+++ mysite/shopping_list/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Item
+
+def create_item(item, amount,date_added,bought):
+    """
+    Create an Item with item, amount, date_added, bought.
+    """
+    date_added = timezone.now()
+    return Item.objects.create(item=item, amount=amount, date_added=date_added, bought=bought)
+
+class ShoppingListIndexViewTests(TestCase):
+    def test_no_items(self):
+        """
+        If no items exist, an appropriate message is displayed.
+        """
+        response = self.client.get("/shopping_list/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No items are available.")
+        self.assertQuerySetEqual(response.context["latest_shopping_list"], [])
+
