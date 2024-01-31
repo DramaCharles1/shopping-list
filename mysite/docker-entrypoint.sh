4a1336759e6da9debf0c4446d081ae6bcdbe7c64
Richard Stuart <richard.niklas.stuart@gmail.com>
2024-01-31 22:41:10 +0100

    Run shopping_list tests
    Run tests specified in shopping_list/tests.py. If the environment variable
    TEST=y the manage.py test command will run instead of runserver. It's
    ok to not specify TEST from shell. README updated.

--- mysite/docker-entrypoint.sh
+++ mysite/docker-entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+#python manage.py runserver 0.0.0.0:8000
+
+if [ "$TEST" == "y" ]; then
+    python manage.py test shopping_list
+else
+    python manage.py runserver 0.0.0.0:8000
+fi
