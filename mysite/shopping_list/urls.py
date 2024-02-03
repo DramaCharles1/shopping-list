08a358a6e9b5aec342eea48149ee5a4ca324c635
Richard Stuart <richard.niklas.stuart@gmail.com>
2024-02-03 22:33:19 +0100

    Add delete endpoint
    Add delete endpoint by adding DestroyItemView in views and
    url in shopping_list/urls.py

--- mysite/shopping_list/urls.py
+++ mysite/shopping_list/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ItemlListCreateView,IndexView, AddItemView
+from .views import ItemlListCreateView,IndexView, AddItemView, DeleteItemView
 
 
 urlpatterns = [
     path("",IndexView.as_view(), name=IndexView.name),
     path("add_item/", AddItemView.as_view(), name=AddItemView.name),
+    path('api/delete-item/<int:pk>/', DeleteItemView.as_view(), name=DeleteItemView.name),
     path('api/add-item/', ItemlListCreateView.as_view(), name=ItemlListCreateView.name),
 ]
