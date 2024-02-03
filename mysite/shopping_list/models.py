3c1447badb609dc413256b3b73261b82e2189f3e
Richard Stuart <richard.niklas.stuart@gmail.com>
2024-02-03 23:20:52 +0100

    Add itemo_no to models. Needed to know what item to delete

--- mysite/shopping_list/models.py
+++ mysite/shopping_list/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 # Create your models here.
 class Item(models.Model):
     item = models.CharField(max_length=200)
+    item_no = models.IntegerField(default=1)
     amount = models.IntegerField()
     date_added = models.DateTimeField()
     bought = models.BooleanField()
