9ff50f2b9905ee38da0aabe6b10e2876ced53be8
Richard Stuart <richard.niklas.stuart@gmail.com>
2024-02-08 21:41:50 +0100

    add DeleteItemView and ApiRoot in shopping_list/urls.py

--- mysite/shopping_list/urls.py
+++ mysite/shopping_list/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ItemlListCreateView,IndexView, AddItemView, DeleteItemView
+from .views import ItemlListCreateView,IndexView, AddItemView, DeleteItemView, ApiRoot
 
 
 urlpatterns = [
     path("",IndexView.as_view(), name="index"),
     path("add_item/", AddItemView.as_view(), name="add_item"),
-    path('api/items/<int:pk>/', DeleteItemView.as_view(), name='delete-item'),
+    path('api/delete_item/<int:pk>/', DeleteItemView.as_view(), name='delete-item'),
     path('Item/', ItemlListCreateView.as_view(), name='item-list-create'),
+    path('api/', ApiRoot.as_view(), name='api-root'),
 ]
