45a97027a40f72f157fdee4f95152b31f59ad80f
Richard Stuart <richard.niklas.stuart@gmail.com>
2024-02-08 23:43:03 +0100

    Set endpoints under /api
    All endpoints should be set under api/. A better implementation would be to separate the
    REST API and shopping_list to different applicaitons.
    
    Set name as attribute in views.

--- mysite/shopping_list/urls.py
+++ mysite/shopping_list/urls.py
@@ -3,7 +3,7 @@ from .views import ItemlListCreateView,IndexView, AddItemView
 
 
 urlpatterns = [
-    path("",IndexView.as_view(), name="index"),
-    path("add_item/", AddItemView.as_view(), name="add_item"),
-    path('Item/', ItemlListCreateView.as_view(), name='item-list-create'),
+    path("",IndexView.as_view(), name=IndexView.name),
+    path("add_item/", AddItemView.as_view(), name=AddItemView.name),
+    path('api/add-item/', ItemlListCreateView.as_view(), name=ItemlListCreateView.name),
 ]
