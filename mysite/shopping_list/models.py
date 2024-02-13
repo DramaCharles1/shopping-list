f5b94d2f25346c01ea587dc856c886c3e0c8ccbf
Richard Stuart <richard.niklas.stuart@gmail.com>
2024-02-13 23:15:00 +0100

    It should not be possible to add an item with negative amount to the database

--- mysite/shopping_list/models.py
+++ mysite/shopping_list/models.py
@@ -1,11 +1,19 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.core.exceptions import ValidationError
+
+def validate_non_negative(value):
+    """
+    It should not be possible to add an item with negative or zero amount to the database.
+    """
+    if value <= 0:
+        raise ValidationError('Amount cannot be zero or non-negative.')
 
 # Create your models here.
 class Item(models.Model):
     item = models.CharField(max_length=200)
-    amount = models.IntegerField()
+    amount = models.IntegerField(validators=[validate_non_negative])
     date_added = models.DateTimeField()
     bought = models.BooleanField()
 
