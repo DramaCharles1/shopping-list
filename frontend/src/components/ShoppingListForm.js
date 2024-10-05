5847e6e5326f910c05fe23b91329ad8db5cbf0b2
Richard Stuart <richard.niklas.stuart@gmail.com>
2024-10-05 23:12:17 +0200

    Use add-item API from frontend
    Use add-item from frontend to add an item to the shopping list database.

--- frontend/src/components/ShoppingListForm.js
+++ frontend/src/components/ShoppingListForm.js
@@ -2,18 +2,41 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { useState } from "react";
+import axios from "axios";
 
 export function ShoppingListForm({ onSubmit }) {
   const [title, setTitle] = useState("");
   const [amount, setAmount] = useState("");
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
     if (title === "" || amount === "") return;
 
-    onSubmit(title, amount);
-    setTitle("");
-    setAmount("");
+    try {
+      // Make a POST request to add the item to the backend
+      const currentDate = new Date();
+      const formattedDate = currentDate.toISOString();
+      const response = await axios.post(
+        "http://127.0.0.1:8000/shopping_list/api/add-item/",
+        {
+          item: title,
+          amount: amount,
+          date_added: formattedDate,
+          bought: false,
+        }
+      );
+
+      // Optionally call onSubmit to update the list in the parent component
+      if (onSubmit) {
+        onSubmit(response.data);
+      }
+
+      // Clear input fields
+      setTitle("");
+      setAmount("");
+    } catch (error) {
+      console.error("Error adding item:", error);
+    }
   }
 
   return (
