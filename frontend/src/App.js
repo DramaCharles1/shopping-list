04c5b56bc4fc97164a9202ac4ad30a280d8d3936
Richard Stuart <richard.niklas.stuart@gmail.com>
2024-11-03 22:25:52 +0100

    Use router-dom for multipage app
    The react router dom enables multipage applications by
    rendering different pages depending on URL.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ShoppingList } from "./pages/ShoppingList";
+import { Pantry } from "./pages/Pantry";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 export default function App() {
-  return <ShoppingList />;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route Component={ShoppingList} path="/shopping-list" />
+          <Route Component={Pantry} path="/pantry" />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
