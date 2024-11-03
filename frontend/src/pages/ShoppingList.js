a0e46206f9a7e09d7e4d43a43baf1e2b62a419d0
Richard Stuart <richard.niklas.stuart@gmail.com>
2024-11-03 21:08:56 +0100

    Create directory for pages
    All pages should be in this directory and they should be built
    by the same components as far as possible.

---
+++ frontend/src/pages/ShoppingList.js
@@ -0,0 +1,108 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { ItemForm } from "../components/ItemForm";
+import { ItemList } from "../components/ItemList";
+import { VerticalMenu } from "../components/VerticalMenu";
+import { Container, Row, Col } from "react-bootstrap";
+
+export function ShoppingList() {
+  const [shoppingList, setShoppingList] = useState([]);
+  const [error, setError] = useState(null); // New state for error
+
+  // Fetch shopping list items from the backend when the component loads
+  useEffect(() => {
+    async function fetchShoppingListItems() {
+      try {
+        const response = await axios.get(
+          "http://127.0.0.1:8000/shopping_list/api/items/"
+        );
+        setShoppingList(response.data);
+      } catch (error) {
+        console.error("Error fetching items:", error);
+        setError("Failed to fetch items");
+      }
+    }
+
+    fetchShoppingListItems();
+  }, []);
+
+  // Function to add a new item to the backend and update state
+  async function addShoppingListItem(title, amount) {
+    try {
+      const currentDate = new Date();
+      const formattedDate = currentDate.toISOString();
+      const response = await axios.post(
+        "http://127.0.0.1:8000/shopping_list/api/add-item/",
+        {
+          item: title,
+          amount: amount,
+          date_added: formattedDate,
+          bought: false,
+        }
+      );
+
+      // Add new item to state based on response from backend
+      setShoppingList((currentShoppingList) => [
+        ...currentShoppingList,
+        response.data,
+      ]);
+    } catch (error) {
+      console.error("Error adding item:", error);
+      setError("Error adding item");
+    }
+  }
+
+  // Function to toggle the bought status of an item
+  async function toggleShoppingListItem(id, boughtStatus) {
+    try {
+      await axios.patch(
+        `http://127.0.0.1:8000/shopping_list/api/update-item/${id}/`,
+        {
+          bought: boughtStatus,
+        }
+      );
+
+      setShoppingList((prevList) =>
+        prevList.map((item) =>
+          item.id === id ? { ...item, bought: boughtStatus } : item
+        )
+      );
+    } catch (error) {
+      console.error("Error updating item:", error);
+      setError("Error updating item");
+    }
+  }
+
+  // Function to delete an item from the shopping list
+  async function deleteShoppingListItem(id) {
+    try {
+      await axios.delete(
+        `http://127.0.0.1:8000/shopping_list/api/delete-item/${id}/`
+      );
+
+      setShoppingList((prevList) => prevList.filter((item) => item.id !== id));
+    } catch (error) {
+      console.error("Error deleting item:", error);
+      setError("Error deleting item");
+    }
+  }
+
+  return (
+    <Container fluid className="h-100">
+      <Row className="h-100">
+        <Col md={3} className="nav-column vh-100">
+          <VerticalMenu />
+        </Col>
+        <Col md={9}>
+          {error && <p className="error-message">{error}</p>}
+          <ItemForm onSubmit={addShoppingListItem} />
+          <ItemList
+            ItemList={shoppingList}
+            toggleListItem={toggleShoppingListItem}
+            deleteListItem={deleteShoppingListItem}
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
