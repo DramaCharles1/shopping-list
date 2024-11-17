95a46a067346a720db363c713caeccfa70eba4d1
Richard Stuart <richard.niklas.stuart@gmail.com>
2024-11-17 22:48:39 +0100

    Specific naming for models, views and imports
    Because the views and models files have been split into new files these
    files need to be updated.

--- mysite/shopping_list/serializers.py
+++ mysite/shopping_list/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
-from .models import Item
+from .models import ShoppingListItem
+from .models import PantryItem
 
 
-class ItemSerializer(serializers.ModelSerializer):
+class ShoppingListItemSerializer(serializers.ModelSerializer):
     date_added = serializers.DateTimeField(format='%Y-%m-%dT%H:%M:%S')
     bought = serializers.BooleanField()
 
     class Meta:
-        model = Item
+        model = ShoppingListItem
         fields = ['id', 'item', 'amount', 'date_added', 'bought']
+
+class PantryItemSerializer(serializers.ModelSerializer):
+    date_added = serializers.DateTimeField(format='%Y-%m-%dT%H:%M:%S')
+
+    class Meta:
+        model = PantryItem
+        fields = ['id', 'item', 'amount', 'date_added']
