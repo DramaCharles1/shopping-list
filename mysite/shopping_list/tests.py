9c40450da7e61307f43fb14c53d3c7a53374dd6f
Richard Stuart <richard.niklas.stuart@gmail.com>
2024-11-17 23:03:01 +0100

    Remove tests in backend for index page
    No longer relevant since this is done and tested in the frontend.

--- mysite/shopping_list/tests.py
+++ mysite/shopping_list/tests.py
@@ -31,27 +31,6 @@ def create_item(item, amount,date_added,bought):
     date_added = timezone.now()
     return ShoppingListItem.objects.create(item=item, amount=amount, date_added=date_added, bought=bought)
 
-class ShoppingListIndexViewTests(TestCase):
-    def test_no_items(self):
-        """
-        If no items exist, an appropriate message is displayed.
-        """
-        response = self.client.get("/shopping_list/")
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "No items are available.")
-        self.assertQuerySetEqual(response.context["latest_shopping_list"], [])
-
-    def test_item_added(self):
-        """
-        If no items exist, an appropriate message is displayed.
-        """
-        item = create_item(item="ketchup", amount=1, date_added=timezone.now(), bought=False)
-        response = self.client.get("/shopping_list/")
-        self.assertQuerySetEqual(
-            response.context["latest_shopping_list"],
-            [item],
-        )
-
 class ShoppingListDeleteItemViewTests(TestCase):
     def test_delete_item_from_list(self):
         """
