80d3d94d88cc5ab1e265865764abbb20c2324c94
Richard Stuart <richard.niklas.stuart@gmail.com>
2024-11-17 22:45:35 +0100

    Split models.py into multiple files
    The models.py file has been split into one file for each page
    to make it more maintainable.

---
+++ mysite/shopping_list/models/pantry_models.py
@@ -0,0 +1,26 @@
+import datetime
+from django.db import models
+from django.utils import timezone
+from django.core.exceptions import ValidationError
+
+def validate_non_negative(value):
+    """
+    It should not be possible to add an item with negative or zero amount to the database.
+    """
+    if value <= 0:
+        raise ValidationError('Amount cannot be zero or non-negative.')
+
+class PantryItem(models.Model):
+    item = models.CharField(max_length=200)
+    amount = models.IntegerField(validators=[validate_non_negative])
+    date_added = models.DateTimeField()
+
+    class Meta:
+        ordering = ('date_added',)
+
+    def __str__(self):
+        return self.item
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.date_added <= now
