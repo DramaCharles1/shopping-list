48c8fd43e402f4e38b1c04bc46995d7acc2d526f
Richard Stuart <richard.niklas.stuart@gmail.com>
2024-11-17 22:52:47 +0100

    Only add api views to urls.py. Remove all ui.

--- mysite/shopping_list/urls.py
+++ mysite/shopping_list/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import ItemlListCreateView,IndexView, AddItemView, DeleteItemView, ItemView, ItemUpdateView
-
+from .views import ShoppingListItemlListCreateView, ShoppingListDeleteItemView, ShoppingListItemView, ShoppingListItemUpdateView, PantryItemlListCreateView, PantryDeleteItemView, PantryItemView, PantryItemUpdateView
 
 urlpatterns = [
-    path("",IndexView.as_view(), name=IndexView.name),
-    path("add_item/", AddItemView.as_view(), name=AddItemView.name),
-    path('api/delete-item/<int:pk>/', DeleteItemView.as_view(), name=DeleteItemView.name),
-    path('api/add-item/', ItemlListCreateView.as_view(), name=ItemlListCreateView.name),
-    path('api/update-item/<int:pk>/', ItemUpdateView.as_view(), name=ItemUpdateView.name),
-    path('api/items/', ItemView.as_view(), name=ItemView.name),
+    path('api/shopping-list/delete-item/<int:pk>/', ShoppingListDeleteItemView.as_view(), name=ShoppingListDeleteItemView.name),
+    path('api/shopping-list/add-item/', ShoppingListItemlListCreateView.as_view(), name=ShoppingListItemlListCreateView.name),
+    path('api/shopping-list/update-item/<int:pk>/', ShoppingListItemUpdateView.as_view(), name=ShoppingListItemUpdateView.name),
+    path('api/shopping-list/items/', ShoppingListItemView.as_view(), name=ShoppingListItemView.name),
+    path('api/pantry/delete-item/<int:pk>/', PantryDeleteItemView.as_view(), name=PantryDeleteItemView.name),
+    path('api/pantry/add-item/', PantryItemlListCreateView.as_view(), name=PantryItemlListCreateView.name),
+    path('api/pantry/update-item/<int:pk>/', PantryItemUpdateView.as_view(), name=PantryItemUpdateView.name),
+    path('api/pantry/items/', PantryItemView.as_view(), name=PantryItemView.name),
 ]
