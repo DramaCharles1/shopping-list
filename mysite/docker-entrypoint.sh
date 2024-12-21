c185d1aac9f44d2825a3de27afa98c184afc8db6
Richard Stuart <richard.niklas.stuart@gmail.com>
2024-12-21 22:06:15 +0100

    Use virtual environment in Django container
    Source the virtual environment created in the Dockerfile.

--- mysite/docker-entrypoint.sh
+++ mysite/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+source /shopping_list/venv/bin/activate
 
 python manage.py migrate
-#python manage.py runserver 0.0.0.0:8000
 
 if [ "$TEST" == "y" ]; then
     python manage.py test shopping_list
