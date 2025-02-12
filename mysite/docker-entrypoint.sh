1171db07ede0c78c37b07b59250230f15b2e3fae
Richard Stuart <richard.niklas.stuart@gmail.com>
2025-02-12 21:56:28 +0100

    Backend entrypoint should do migrate as default
    If not TEST or DEVELOPMENT are sent, only migrate should be done.
    This is needed for production. The virtual environment is activated.

--- mysite/docker-entrypoint.sh
+++ mysite/docker-entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-source /shopping_list/venv/bin/activate
+source /venv_env/bin/activate
 
 python manage.py migrate
 
 if [ "$TEST" == "y" ]; then
     python manage.py test shopping_list
-else
-    python manage.py runserver 0.0.0.0:8000
+fi
+if [ "$DEVELOPMENT" == "y" ]; then
+    python manage.py test shopping_list
 fi
