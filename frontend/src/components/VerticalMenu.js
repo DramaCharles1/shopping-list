4c01ee35bff41d7fa77215d206cd49d75ad46d52
Richard Stuart <richard.niklas.stuart@gmail.com>
2025-02-12 21:42:41 +0100

    Map links to basename /shopping-list
    All links should behave like React router and use basename
    /shopping-list to correctly navigate to the pages.

--- frontend/src/components/VerticalMenu.js
+++ frontend/src/components/VerticalMenu.js
@@ -1,16 +1,17 @@
 import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
 
 export function VerticalMenu() {
   return (
     <div className="navbar-container">
       <Nav defaultActiveKey="/shopping-list" className="custom-nav flex-column">
-        <Nav.Link href="/shopping-list" className="custom-link">
+        <Nav.Link as={Link} to="/" className="custom-link">
           Current shopping list
         </Nav.Link>
-        <Nav.Link href="/pantry" className="custom-link">
+        <Nav.Link as={Link} to="/pantry" className="custom-link">
           Pantry
         </Nav.Link>
-        <Nav.Link eventKey="link-recipe" className="custom-link">
+        <Nav.Link as={Link} to="/recipe" className="custom-link">
           Recipe
         </Nav.Link>
       </Nav>
